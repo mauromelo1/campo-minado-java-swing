35349bf9b7667fe01140a3dd2631755e651cbcf1
Mauro Melo <mauromelo1@gmail.com>
2021-03-30 18:40:00 -0300

    visao.Temp para ganhar

--- src/com/meujogo/cm/visao/Temp.java
+++ src/com/meujogo/cm/visao/Temp.java
@@ -7,7 +7,15 @@ public class Temp {
 	public static void main(String[] args) {
 		
 		Tabuleiro tabuleiro = new Tabuleiro(3, 3, 9);
-		tabuleiro.abrir(2, 2);
+		tabuleiro.alternarMarcacao(0, 0);
+		tabuleiro.alternarMarcacao(0, 1);
+		tabuleiro.alternarMarcacao(0, 2);
+		tabuleiro.alternarMarcacao(1, 0);
+		tabuleiro.alternarMarcacao(1, 1);
+		tabuleiro.alternarMarcacao(1, 2);
+		tabuleiro.alternarMarcacao(2, 0);
+		tabuleiro.alternarMarcacao(2, 1);
+		tabuleiro.alternarMarcacao(2, 2);
 	}
 
 }
