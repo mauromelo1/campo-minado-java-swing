92092757b95460d675140cbf4dc2614ce01d9cfd
Mauro Melo <mauromelo1@gmail.com>
2021-03-30 15:58:19 -0300

    Projeto criado

---
+++ src/com/meujogo/cm/modelo/Campo.java
@@ -0,0 +1,122 @@
+package com.meujogo.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campo {
+
+	private final int linhaCampo;
+	private final int colunaCampo;
+
+	// Caracteristicas do Campo
+	private boolean abertoCampo = false;
+	private boolean minadoCampo = false;
+	private boolean marcadoCampo = false;
+
+	private List<Campo> vizinhos = new ArrayList<>();
+
+	public Campo(int linha, int coluna) {
+		this.linhaCampo = linha;
+		this.colunaCampo = coluna;
+	} // fim construtor
+
+	boolean adicionarVizinhoCampo(Campo candidatoVizinho) {
+		boolean linhaDiferente = (linhaCampo != candidatoVizinho.linhaCampo);
+		boolean colunaDiferente = (colunaCampo != candidatoVizinho.colunaCampo);
+		boolean diagonal = linhaDiferente && colunaDiferente;
+
+		int deltaLinha = Math.abs(linhaCampo - candidatoVizinho.linhaCampo);
+		int deltaColuna = Math.abs(colunaCampo - candidatoVizinho.colunaCampo);
+		int deltaGeral = deltaLinha + deltaColuna;
+
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(candidatoVizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(candidatoVizinho);
+			return true;
+		} else {
+			return false;
+		}
+	} // fim adicionarVizinhoCampo
+
+	void alternarMarcacaoCampo() {
+		if (!abertoCampo) {
+			marcadoCampo = !marcadoCampo;
+		}
+	} // fim alternarMarcacaoCampo
+
+	boolean abrirCampo() {
+		if (!abertoCampo && !marcadoCampo) {
+			abertoCampo = true;
+
+			if (minadoCampo) {
+				// TODO implementar nova versão
+			} // fim if do campoMinado
+
+			if (vizinhancaSeguraCampo()) {
+				vizinhos.forEach(v -> v.abrirCampo());
+			} // fim if vizinhancaSegura
+
+			return true;
+
+		} // fim if !abertoCampo && !marcadoCampo
+		else {
+			return false;
+		}
+	} // fim abrirCampo
+
+	boolean vizinhancaSeguraCampo() {
+		return vizinhos.stream().noneMatch(v -> v.minadoCampo);
+	} // fim vizinhancaSeguraCampo
+
+	void minarCampo() {
+		minadoCampo = true;
+	} // fim minarCampo
+
+	public boolean isMinadoCampo() {
+		return minadoCampo;
+	} // fim isMinadoCampo
+
+	public boolean isMarcadoCampo() {
+		return marcadoCampo;
+	} // fim isMarcadoCampo
+
+	void setAbertoCampo(boolean aberto) {
+		this.abertoCampo = aberto;
+	} // setAbertoCampo
+
+	public boolean isAbertoCampo() {
+		return abertoCampo;
+	} // fim isAbertoCampo
+
+	public boolean isFechadoCampo() {
+		return !isAbertoCampo();
+	} // fim isFechadoCampo
+
+	public int getLinhaCampo() {
+		return linhaCampo;
+	} // fim getLinhaCampo
+
+	public int getColunaCampo() {
+		return colunaCampo;
+	} // fim getColunaCampo
+
+	boolean objetivoAlcancadoCampo() {
+		boolean desvendado = !minadoCampo && abertoCampo;
+		boolean protegido = minadoCampo && marcadoCampo;
+
+		return desvendado || protegido;
+	} // fim objetivoAlcancadoCampo
+
+	long minasNaVizinhancaCampo() {
+		return vizinhos.stream().filter(v -> v.minadoCampo).count();
+	} // fim minasNavizinhancaCampo
+
+	void reiniciarCampo() {
+		abertoCampo = false;
+		minadoCampo = false;
+		marcadoCampo = false;
+	} // fim reiniciarCampo
+
+} // fim da Class Campo
---
+++ src/com/meujogo/cm/modelo/Tabuleiro.java
@@ -0,0 +1,78 @@
+package com.meujogo.cm.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Tabuleiro {
+
+	private int linhasTabuleiro;
+	private int colunasTabuleiro;
+	private int minasTabuleiro;
+
+	private final List<Campo> campos = new ArrayList<>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhasTabuleiro = linhas;
+		this.colunasTabuleiro = colunas;
+		this.minasTabuleiro = minas;
+
+		gerarCamposTabuleiro();
+		associarVizinhosTabuleiro();
+		sortearMinasTabuleiro();
+
+	} // fim construtor
+
+	public void abrir(int linha, int coluna) {
+		try {
+			campos.parallelStream().filter(c -> c.getLinhaCampo() == linha && c.getColunaCampo() == coluna).findFirst()
+					.ifPresent(c -> c.abrirCampo());
+		} catch (Exception e) {
+			// FIXME ajustar a implementação do método abrir
+			campos.forEach(c -> c.setAbertoCampo(true));
+			throw e;
+		}
+	} // fim abrir
+
+	public void alternarMarcacao(int linha, int coluna) {
+		campos.parallelStream().filter(c -> c.getLinhaCampo() == linha && c.getColunaCampo() == coluna).findFirst()
+				.ifPresent(c -> c.alternarMarcacaoCampo());
+	} // fim AlternarMarcacao
+
+	private void gerarCamposTabuleiro() {
+
+		for (int l = 0; l < linhasTabuleiro; l++) {
+			for (int c = 0; c < colunasTabuleiro; c++) {
+				campos.add(new Campo(l, c));
+			} // for c
+		} // for l
+	} // fim gerarCamposTabuleiro
+
+	private void associarVizinhosTabuleiro() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinhoCampo(c2);
+			} // for c2
+		} // for c1
+	} // fim associarVizinhosTabuleiro
+
+	private void sortearMinasTabuleiro() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinadoCampo();
+
+		do {
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minarCampo();
+			minasArmadas = campos.stream().filter(minado).count();
+		} while (minasArmadas < minasTabuleiro);
+	} // sortearMinasTabuleiro
+
+	public boolean objetivoAlcancadoTabuleiro() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancadoCampo());
+	} // fim objetivoAlcancadoTabuleiro
+
+	public void reiniciarTabuleiro() {
+		campos.stream().forEach(c -> c.reiniciarCampo());
+		sortearMinasTabuleiro();
+	} // fim reiniciarTabuleiro
+}
