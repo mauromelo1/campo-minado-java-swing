ddcfc5bd0399ce8d5924a8b5541481a537bc5c04
Mauro Melo <mauromelo1@gmail.com>
2021-04-09 14:15:09 -0300

    Interface grafica BotaoCampo

--- src/com/meujogo/cm/visao/BotaoCampo.java
+++ src/com/meujogo/cm/visao/BotaoCampo.java
@@ -1,13 +1,68 @@
 package com.meujogo.cm.visao;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 import com.meujogo.cm.modelo.Campo;
+import com.meujogo.cm.modelo.CampoEvento;
+import com.meujogo.cm.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton{
+public class BotaoCampo extends JButton implements CampoObservador {
+
+	private final Color BG_PADRAO = new Color(184, 184, 184);
+	private final Color BG_MARCAR = new Color(8, 179, 247);
+	private final Color BG_EXPLODIR = new Color(189, 66, 68);
+	private final Color TEXTO_VERDE = new Color(0, 100, 0);
+	private Campo campo;
 
 	public BotaoCampo(Campo campo) {
-	
+		this.campo = campo;
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createBevelBorder(0));
+		campo.registrarObservador(this);
+	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch (evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+
+		default:
+			aplicarEstiloPadrao();
+		}
+
+	}
+
+	private void aplicarEstiloAbrir() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void aplicarEstiloMarcar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void aplicarEstiloExplodir() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void aplicarEstiloPadrao() {
+		// TODO Auto-generated method stub
+
 	}
 }
